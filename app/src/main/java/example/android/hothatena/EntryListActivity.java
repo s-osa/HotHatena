ea9f0d93871a0a51f4e1d2e13d1504c716687d7f
OSA Shunsuke <hhelibebcnofnenamg@gmail.com>
2014-08-29 16:00:12 +0900

    Add internet access permission to get RSS.

--- app/src/main/java/example/android/hothatena/EntryListActivity.java
+++ app/src/main/java/example/android/hothatena/EntryListActivity.java
@@ -2,11 +2,12 @@ package example.android.hothatena;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ListView;
-
-import java.util.List;
+import android.widget.Toast;
+import org.apache.http.Header;
+import rejasupotaro.asyncrssclient.AsyncRssClient;
+import rejasupotaro.asyncrssclient.AsyncRssResponseHandler;
+import rejasupotaro.asyncrssclient.RssFeed;
 
 
 public class EntryListActivity extends Activity {
@@ -15,6 +16,19 @@ public class EntryListActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entry_list);
+
+        AsyncRssClient client = new AsyncRssClient();
+        client.read("http://b.hatena.ne.jp/hotentry?mode=rss", new AsyncRssResponseHandler() {
+            @Override
+            public void onSuccess(RssFeed rssFeed) {
+                Toast.makeText(EntryListActivity.this, "Successfully loaded.", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(int i, Header[] headers, byte[] bytes, Throwable throwable) {
+                Toast.makeText(EntryListActivity.this, "Failed to read RSS.", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         ListView listView = (ListView) findViewById(R.id.lv_entries);
         listView.setEmptyView(findViewById(R.id.empty));
