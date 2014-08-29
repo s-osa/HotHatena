906ddfaa9ddb27078726097b85d9f3e9756b768d
OSA Shunsuke <hhelibebcnofnenamg@gmail.com>
2014-08-29 17:03:10 +0900

    List hot entries.

--- app/src/main/java/example/android/hothatena/EntryListActivity.java
+++ app/src/main/java/example/android/hothatena/EntryListActivity.java
@@ -2,13 +2,15 @@ package example.android.hothatena;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 import org.apache.http.Header;
+import java.util.List;
 import rejasupotaro.asyncrssclient.AsyncRssClient;
 import rejasupotaro.asyncrssclient.AsyncRssResponseHandler;
 import rejasupotaro.asyncrssclient.RssFeed;
-
+import rejasupotaro.asyncrssclient.RssItem;
 
 public class EntryListActivity extends Activity {
 
@@ -21,7 +23,17 @@ public class EntryListActivity extends Activity {
         client.read("http://b.hatena.ne.jp/hotentry?mode=rss", new AsyncRssResponseHandler() {
             @Override
             public void onSuccess(RssFeed rssFeed) {
-                Toast.makeText(EntryListActivity.this, "Successfully loaded.", Toast.LENGTH_SHORT).show();
+                ArrayAdapter adapter = new ArrayAdapter(EntryListActivity.this, android.R.layout.simple_list_item_1);
+
+                ListView listView = (ListView) findViewById(R.id.lv_entries);
+                listView.setAdapter(adapter);
+
+                List<RssItem> itemList = rssFeed.getRssItemList();
+
+                for (RssItem rssItem: itemList) {
+                    String title = rssItem.getTitle();
+                    adapter.add(title);
+                }
             }
 
             @Override
